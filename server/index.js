ab50c453076ef9f7237ac0c82585794485609612
Jakey Wakey <charliebmalishka@gmail.com>
2022-05-27 22:37:22 -0400

    comment server

--- server/index.js
+++ server/index.js
@@ -21,14 +21,25 @@ app.get('/', (req,res) => {
 });
 
 /* broadcast */
-try {
-	app.listen(process.env.PORT || 4000);
-	const timestamp = new Date();
-	let month = convertMonth(timestamp.getMonth(), 'full');
-	month = month[0].toUpperCase() + month.slice(1);
-	// display success
-	console.log(`Server activated --> ${month} ${timestamp.getDay()}, ${timestamp.getFullYear()} @ ${timestamp.getHours()}:${timestamp.getMinutes()}`);
-}
-catch (err) {
-	console.log(err);
-}
+const broadcastServer = (() => {
+	try {
+		// okug in to port
+		app.listen(process.env.PORT || 4000);
+		// create readable timestamp for logging
+		///// todo /////
+		// make the code block below into a 'common' function
+		const timestamp = new Date();
+		let month = convertMonth(timestamp.getMonth(), 'full');
+		month = month[0].toUpperCase() + month.slice(1);
+		const date = `${month} ${timestamp.getDay()}, ${timestamp.getFullYear()}`;
+		const time = `${timestamp.getHours()}:${timestamp.getMinutes()}`;
+		// display success
+		console.log(`Server activated --> ${date} @ ${time}`);
+	}
+	catch (err) {
+		console.log(err);
+	}
+	///// todo /////
+	// check port number
+	// if 4000, determine reason for default val
+})();
