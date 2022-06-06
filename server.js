ac9cf0bf00eb5e724ba77cd4bf9cbcd9bfa3ba83
Jakey Wakey <charliebmalishka@gmail.com>
2022-06-05 21:46:29 -0400

    user api routes and reg page link up

--- server.js
+++ server.js
@@ -9,45 +9,30 @@ dotenv.config()
 /* config */
 const cors = require('cors')
 app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.set('views', './views')
 
+/* route imports */
+const authRouter = require('./routes/authRoutes.js')
+const userRouter = require('./routes/userRoutes.js')
+
 /* init and connect to database */
 const mongo = require('mongoose')
 mongo.connect(process.env.DB)
-	.then(() => { console.log('...data stream live') })
-	.catch(err => console.log(err));
+.then(() => { console.log('...data stream live') })
+.catch(err => console.log(err));
 
 /* routing */
-app.get('/', (req,res) => {
- 	res.render('home', {data:{}, pageTitle:'Home'})
-})
+app.use('/api/users', userRouter)
+app.use('/auth', authRouter)
 
-app.get('/users', (req, res) => {
-	let x = [
-		{
-			name: 'jay',
-			age: 37,
-			image: '/fake-data/images/man.png'
-		},
-		{
-			name: 'anna',
-			age: 36,
-			image: '/fake-data/images/woman.png'
-		},
-		{
-			name: 'sophia',
-			age: 9,
-			image: '/fake-data/images/girl.png'
-		},
-		{
-			name: 'angela',
-			age: 0,
-			image: '/fake-data/images/baby.png'
-		}
-	]
-	res.render('users', {data:x,pageTitle:'User List'})
+/* home page */
+app.get('/', (req,res) => {
+	req
+ 	res.render('home', {data:{}, pageTitle:'Home'})
 })
 
 /* broadcast */
---
+++ users.js
@@ -0,0 +1,24 @@
+const fakeUsers = [
+	{
+		name: 'jay',
+		age: 37,
+		image: '/fake-data/images/man.png'
+	},
+	{
+		name: 'anna',
+		age: 36,
+		image: '/fake-data/images/woman.png'
+	},
+	{
+		name: 'sophia',
+		age: 9,
+		image: '/fake-data/images/girl.png'
+	},
+	{
+		name: 'angela',
+		age: 0,
+		image: '/fake-data/images/baby.png'
+	}
+]
+
+module.exports = fakeUsers
